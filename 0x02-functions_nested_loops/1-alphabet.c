e1a4972b98addc57ebd87222378cc588e6a13184
faji-75 <fajisultan@gmail.com>
2021-06-22 18:51:12 +0000

    if stat

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
- #include <stdio.h>
- /**
+#include <stdio.h>
+/**
 * main - main block
 * Description: Use `putchar` function to print the alphabet in lowercase.
 * Return: 0
@@ -7,12 +7,14 @@
 int main(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
-		{
-			putchar(c);
-			c++;
-		}
-			putchar('\n');
-			return (0);
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
 }
 
