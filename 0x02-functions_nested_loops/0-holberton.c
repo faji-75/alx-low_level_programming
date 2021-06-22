596f9384b959bfb9eeb79dcea15b5f314840c229
faji-75 <fajisultan@gmail.com>
2021-06-22 17:53:20 +0000

    if stat

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+
+	char h = 'H';
+	char o = 'o';
+	char l = 'l';
+	char b = 'b';
+	char e = 'e'; 
+	char r = 'r';
+	char t = 't';
+	char o = 'o';
+	char n = 'n';
+
+		{
+			putchar(h);
+			putchar(o);
+			putchar(l);
+			putchar(b);
+			putchar(e);
+			putchar(r);
+			putchar(t);
+			putchar(o);
+			putchar(n);
+		}
+		 putchar('\n');
+
+		 return (0);
+	
+		
+}
