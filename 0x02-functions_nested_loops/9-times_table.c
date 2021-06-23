73331c360a47d5c15e4f245ff26318d7516fdb8b
faji-75 <fajisultan@gmail.com>
2021-06-23 14:20:12 +0000

    loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,21 @@
 #include "holberton.h"
 /**
-  * jack_bauer - Print every minute of the day of Jack Bauer
+  * times_table - Print times table of nine
   * Description: Start from 00:00 to 23:59
   */
 void times_table(void)
 {
+	int x = 0;
+	int y = 0;
 
+	while (x <= 9)
+	{
+		while (y <= 9)
+		{
+			_putchar(x * y);
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
