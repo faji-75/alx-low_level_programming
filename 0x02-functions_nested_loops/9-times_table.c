b5c89170e7af0e47dab6e3ee1dda2581cd08b198
faji-75 <fajisultan@gmail.com>
2021-06-23 14:34:48 +0000

    loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 /**
-  * times_table - Print times table of nine
-  * Description: Start from 00:00 to 23:59
+  * times_table - Print times table of nine.
   */
 void times_table(void)
 {
 	int x = 0;
 	int y = 0;
 	int z;
-	while (x <= 9)
+	
+	while (x < 10)
 	{
-		while (y <= 9)
+		while (y < 10)
 		{
 			z = x * y;
 			_putchar(z);
