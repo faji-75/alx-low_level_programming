67847f7ea3b6fd568ace94274dbe8a2a23fa6efd
faji-75 <fajisultan@gmail.com>
2021-06-23 13:30:47 +0000

    loop

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 /**
- * _islower - function that checks for lowercase character.
+ * print_sign - function give the sign.
+ * @n: integert type.
  * Return: 1 for lowercase otherwise 0.
  */
 
