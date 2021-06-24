0d542bd3fc7a66ccae81bba3331cea4e4c0a599e
faji-75 <fajisultan@gmail.com>
2021-06-24 20:38:34 +0000

    all

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,39 @@
 #include "holberton.h"
+
+/**
+  * print_triangle - prints triangle of #
+  * @size: int type
+  */
+
+void print_triangle(int size)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	b = size;
+	d = size;
+
+	while (size > 0)
+	{
+		b--;
+		a = b;
+
+		while (a > 0)
+		{
+			a--;
+			_putchar(' ');
+
+		}
+
+		c = d - b;
+		while (c > 0)
+		{
+			_putchar('35');
+			c--;
+		}
+		_putchar('\n');
+		size--;
+	}
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,17 @@
-#include "holberton.h"
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	char Fizz;
+	char Buzz;
+	char FizzBuzz;
+	int a;
+
+	a = 1;
+	while (a < 101)
+	{
+		if (a % 3 = 0)
+		{
+			if (a % 5 = 0)
