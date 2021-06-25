9700b34605e573f75748f83a4233f434842e939f
faji-75 <fajisultan@gmail.com>
2021-06-25 06:11:25 +0000

    all

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,16 +11,21 @@ void print_square(int size)
 	int b;
 
 	b = size;
+	if (size < 1)
+	{
+		_putchar('\n');
+	}
 
 	while (size > 0)
 	{
 		a = b;
 		while (a > 0)
 		{
-			_putchar(35);
+			_putchar('#');
 			a--;
 		}
 		_putchar('\n');
 		size--;
 	}
+
 }
