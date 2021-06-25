49144f95c6cab0a858e5d3b3c65baf221d2bf8b9
faji-75 <fajisultan@gmail.com>
2021-06-25 11:46:55 +0000

    all

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-/**
-*	while (i < 10)
-*	{
-*		putchar(i);
-*	}
-*/
+
+  /* commented -	while (i < 10)*/
+  /*	{ */
+  /* @\o/: comment	putchar(i);*/
+  /*	} */
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
