e5e26bba35c232ded50c20ec13b2ee8403ea8115
faji-75 <fajisultan@gmail.com>
2021-06-28 12:17:33 +0000

    pointer array string

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include <holberton.h>
-
-
-
+/**
+  * reset_to_98 - function which rest the valu of n to 98.
+  * @n: integer type.
+  */
 void reset_to_98(int *n)
 {
 	int *p;
+
 	p = &n;
 
 	*p = 98;
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,9 @@
 #include <holberton.h>
-
-
-
+/**
+  * swap_int - function to swap to integers
+  * @a: int type
+  * @b: int type
+  */
 void swap_int(int *a, int *b)
 {
 	int *x;
