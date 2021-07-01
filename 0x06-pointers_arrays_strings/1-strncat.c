751cba1f783894ab249842800eeba3c4299f9b98
faji-75 <fajisultan@gmail.com>
2021-07-01 19:45:11 +0000

    more pointer array string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,44 +1,23 @@
 #include "holberton.h"
 
 /**
-  * _strncat - function appends the src string to the dest string
-  * @dest: char type
-  * @src: char type
-  * @n: int type
-  * Return: returns dest
-  */
+ * _strncat - concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting `dest`
+ */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	char *x;
-	char *y;
-	int a = 0;
-	int b;
+	int i, c;
 
-	x = dest;
-	y = src;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-	while (*x != '\0')
+	for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
 	{
-		dest[a] = *x;
-		x++;
-		a++;
-	}
-
-
-	dest[a] = ' ';
-	b = a + n;
-
-
-	while (*y != '\0' && a < b)
-	{
-		dest[a] = *y;
-		y++;
-		a++;
-	}
-
-	if (a < b)
-	{
-	dest[a] = '\0';
+		dest[i] = src[c];
 	}
 	return (dest);
 }
