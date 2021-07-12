2437aa441b1ed0f1ea103a7bfd1e4308e649014a
faji-75 <fajisultan@gmail.com>
2021-07-12 18:20:11 +0000

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,13 +5,10 @@
   * @argv: char type
   * Return: return 0
   */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__))argc, char *argv[])
 
 {
-	int i;
-
-	i = argc - 1;
-	printf("%s\n", argv[i]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
