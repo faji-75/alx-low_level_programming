4c24d570ac6a0f6955a3cb09467008651eedb3c0
faji-75 <fajisultan@gmail.com>
2021-07-12 19:54:46 +0000

    arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
   * @argv: char type
   * Return: returns zero
   */
-int main (int argc, char __attribute__((__unused__))*argv[])
+int main(int argc, char __attribute__((__unused__))*argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
