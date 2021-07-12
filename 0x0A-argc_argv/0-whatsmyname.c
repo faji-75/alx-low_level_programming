d1af18e395ff028469d52e705e737eaa756a96c3
faji-75 <fajisultan@gmail.com>
2021-07-12 15:05:37 +0000

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-
-int main (int argc, char *argv[])
+/**
+  * main - function rename it self
+  * @argc: int type
+  * @argv: char type
+  * Return: return 0
+  */
+int main(int argc, char *argv[])
 
 {
 if (argc > 0)
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 
-	return 0;
+	return (0);
 }
