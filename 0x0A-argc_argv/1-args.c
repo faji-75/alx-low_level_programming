2add547c9d68148ab8bf4463b199c00cf33f884a
faji-75 <fajisultan@gmail.com>
2021-07-12 18:51:07 +0000

    arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,22 +7,6 @@
   */
 int main (int argc, char __attribute__((__unused__))*argv[])
 {
-	int count;
-
-
-	if (argc > 1)
-	{
-		for (count = 0; count < argc; count++)
-		{
-		}
-		count--;	
-		printf("%d\n", count);
-		
-	}
-	else
-	{
-		printf("%d\n", 0);
-	}
-
+	printf("%d\n", argc - 1);
 	return (0);
 }
