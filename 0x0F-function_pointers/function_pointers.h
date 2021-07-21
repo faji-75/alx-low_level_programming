f849c977fa69cc7f2548ae84f8e969f699f82704
faji-75 <fajisultan@gmail.com>
2021-07-21 20:01:06 +0000

    function pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,11 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-int _putchar(char c);
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+#include <stddef.h>
+
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif /* _HOLBERTON_H_ */
+
+#endif /* _FUNCTION_POINTERS_H_ */
